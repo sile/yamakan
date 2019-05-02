626ebd0d6da593062b8e53d5ffa325ca0c156d12
Takeru Ohta <phjgt308@gmail.com>
2019-05-02 22:31:32 +0900

    Add `Leveled`

--- src/budget.rs
+++ src/budget.rs
@@ -1,4 +1,5 @@
 //! Budget for evaluating parameters.
+use std::cmp::{Ordering, Reverse};
 
 /// Budget.
 #[derive(Debug, Clone, Copy)]
@@ -43,7 +44,7 @@ impl Budget {
     }
 }
 
-/// An object with a specific budget.
+/// An object which has a specific budget.
 #[derive(Debug, Clone, Copy)]
 pub struct Budgeted<T> {
     budget: Budget,
@@ -78,5 +79,55 @@ impl<T> Budgeted<T> {
     /// Consumes the `Budgeted`, returning the wrapped object.
     pub fn into_inner(self) -> T {
         self.inner
+    }
+}
+
+/// An object leveled by its resource consumption.
+///
+/// Note that this is provided with the intention of being used for optimizations of minimizing direction.
+/// That is, the higher the level, the lower the order by `std::cmp::Ordering`.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Leveled<T> {
+    level: u64,
+    inner: T,
+}
+impl<T> Leveled<T> {
+    /// Makes a new `Leveled` instance.
+    pub const fn new(level: u64, inner: T) -> Self {
+        Self { level, inner }
+    }
+
+    /// Returns the level of this instance.
+    pub const fn level(&self) -> u64 {
+        self.level
+    }
+
+    /// Returns a reference to the underlying object.
+    pub const fn get(&self) -> &T {
+        &self.inner
+    }
+
+    /// Returns a mutable reference to the underlying object.
+    pub fn get_mut(&mut self) -> &mut T {
+        &mut self.inner
+    }
+
+    /// Consumes the `Leveled`, returning the wrapped object.
+    pub fn into_inner(self) -> T {
+        self.inner
+    }
+
+    fn to_tuple(&self) -> (Reverse<u64>, &T) {
+        (Reverse(self.level), &self.inner)
+    }
+}
+impl<T: PartialOrd> PartialOrd for Leveled<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.to_tuple().partial_cmp(&other.to_tuple())
+    }
+}
+impl<T: Ord> Ord for Leveled<T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.to_tuple().cmp(&other.to_tuple())
     }
 }
